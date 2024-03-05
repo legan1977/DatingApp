e52af59a328c4811349be563dc5734fc672b62ce
Willy Nagel <willy.nagel@cpp.canon>
2024-03-05 22:41:30 +0100

    end of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +12,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
---
+++ set-node-path.ps1
@@ -0,0 +1,3 @@
+Set-Location -Path c:\OceTools\Nodejs\18.16.0-R01\
+$Env:PATH += ";$pwd"
+Set-Location -Path c:\Data\Code\Udemy\DatingApp\client
